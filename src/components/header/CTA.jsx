e9b105f0a5fa79cedfb33518afdd922636d04a25
Eden-Ismah-Moshe <eden12012@gmail.com>
2024-07-30 09:32:41 +0300

    update header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,15 +4,7 @@ const CTA = () => {
   return (
     <>
       <div className="cta">
-        <a
-          href="https://drive.google.com/file/d/1WPOxxu-nWKnNRE7MVfgMECp8pN2fIuU2/view?usp=drive_link"
-          download
-          className="btn btn-primary"
-        >
-          Download CV
-        </a>
-
-        <a href="#contact" className="btn download-btn">
+        <a href="#contact" className="btn btn-primary">
           Let's Talk
         </a>
 
@@ -25,3 +17,14 @@ const CTA = () => {
 };
 
 export default CTA;
+
+/*
+ <a
+          href="https://drive.google.com/file/d/1WPOxxu-nWKnNRE7MVfgMECp8pN2fIuU2/view?usp=drive_link"
+          download
+          className="btn btn-primary"
+        >
+          Download CV
+        </a>
+
+*/
